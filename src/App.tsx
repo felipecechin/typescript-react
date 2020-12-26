8c80eaf712251784863f6924350a19f4d8032f97
Felipe <ficechin@hotmail.com>
2020-12-26 14:27:04 -0300

    Project concluded.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     return (
         <div className="App">
             <Contador valorInicial={321}/>
+            <Contador/>
         </div>
     );
 }
--- src/componentes/Contador.tsx
+++ src/componentes/Contador.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ContadorValor from "./ContadorValor";
+import logRender from "../decorators/logRender";
 
 interface IContadorProps {
     valorInicial?: number
@@ -9,12 +10,23 @@ interface IContadorState {
     valor: number
 }
 
+@logRender
 export default class Contador extends Component<IContadorProps, IContadorState> {
     public state = {valor: this.props.valorInicial || 0}
 
+    private setValor = (delta: number) => {
+        this.setState({
+            valor: this.state.valor + delta
+        })
+    }
+
     render() {
         return (
-            <ContadorValor contador={this.state.valor}/>
+            <div>
+                <ContadorValor contador={this.state.valor}/>
+                <button onClick={() => this.setValor(10)}>+</button>
+                <button onClick={() => this.setValor(-10)}>-</button>
+            </div>
         )
     }
 }
